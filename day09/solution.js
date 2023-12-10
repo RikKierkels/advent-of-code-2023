92bd62109192f0a1955aedc3e43470915373cc21
Rik Kierkels <kierkels.r@gmail.com>
2023-12-10 21:52:33 +0100

    Solve day 9

--- day09/solution.js
+++ day09/solution.js
@@ -1,5 +1,35 @@
 const input = require('../input');
 const log = console.log;
+const sum = (a, b) => a + b;
 
-log(`Solution pt.1 ${1}`);
-log(`Solution pt.2 ${2}`);
+const predict = (sequence, predictFn) => {
+  const first = sequence[0];
+  if (sequence.every((value) => value === first)) return first;
+
+  const differences = [];
+  for (let i = 1; i < sequence.length; ++i) {
+    differences.push(sequence[i] - sequence[i - 1]);
+  }
+
+  const prediction = predict(differences, predictFn);
+  return predictFn(sequence, prediction);
+};
+
+const histories = input(__dirname, './input.txt')
+  .split('\n')
+  .map((history) => history.split(' '))
+  .map((history) => history.map((n) => +n));
+
+const sumOfNextValues = histories
+  .map((history) =>
+    predict(history, (sequence, prediction) => sequence.at(-1) + prediction),
+  )
+  .reduce(sum);
+log(`Solution pt.1 ${sumOfNextValues}`);
+
+const sumOfPreviousValues = histories
+  .map((history) =>
+    predict(history, (sequence, prediction) => sequence[0] - prediction),
+  )
+  .reduce(sum);
+log(`Solution pt.2 ${sumOfPreviousValues}`);
